7bbdc3b9d1da220bc90c5788c1fb1ff36d63744e
koushik ram <117310312+Itskoushik@users.noreply.github.com>
2024-02-08 00:42:43 +0530

    initial setup done

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,23 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 
+const db=mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database :'details'
+})
+
+app.get('/users', (req, res) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql , (err, data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+        })
+    })
+
+
+
 app.get( '/', (re, res) => {
     return res.json("from backend side");
 })
